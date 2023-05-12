ec03bc246f6ef7635b93cdb8e336ca8ae397de18
joaofp <jvfpdaemoon1@gmail.com>
2023-05-12 01:52:48 -0300

    feat: template

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 dotenv.config();
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
     res.status(200).json({
         status: 'success'
     });
